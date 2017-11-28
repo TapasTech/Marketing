7e90bf9a707198a428a9715eb6dbb86164c49b35
Kou Guandong <gdkou90@gmail.com>
2017-11-28 11:21:32 +0800

    chore: updated agenda and VIPs

--- bin/build.sh
+++
@@ -1,7 +0,0 @@
-DIR=./build
-if [ -d "$DIR" ]; then
-    printf '%s\n' "Removing Lock ($DIR)"
-    rm -rf "$DIR"
-fi
-
-./node_modules/.bin/webpack --config webpack.config.prod.js --progress --production --color 
